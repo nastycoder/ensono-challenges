5c2e7c74a9c809a65a94f46baa7d118708c8cb6e
Uriah Adams <adams.uriah@gmail.com>
2016-04-24 19:53:18 -0500

    fixed card layout

--- contacts/app/contact/contact.component.ts
+++ contacts/app/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input} from 'angular2/core';
 import {MdButton} from '@angular2-material/button'
 import {MD_CARD_DIRECTIVES, MdCard} from '@angular2-material/card'
+import {MD_LIST_DIRECTIVES} from '@angular2-material/list'
 
 export class Contact {
   id: number
@@ -15,7 +16,7 @@ export class Contact {
     selector: 'contact',
     templateUrl: 'app/contact/contact.component.html',
     styleUrls: ['app/contact/contact.component.css'],
-    directives: [MD_CARD_DIRECTIVES, MdButton]
+    directives: [MD_CARD_DIRECTIVES, MdButton, MD_LIST_DIRECTIVES]
 })
 export class ContactComponent {
   @Input() contact: Contact
